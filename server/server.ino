d919e8a9aee4ecf884fa4714218be1a3d4761c61
Aditya Malusare <aditya.malusare@gmail.com>
2017-09-23 22:49:50 +0530

    Add server.ino
    A simple data pipe

---
+++ server/server.ino
@@ -0,0 +1,34 @@
+/*
+PINOUT
+8  - VCC #2 
+9  - TRIG 1
+10 - ECHO 1
+11 - TRIG 2
+12 - ECHO 2
+*/
+#include <Ultrasonic.h>
+
+// We need to make our own library for improving triggering and resolution
+//Ultrasonic u1(9, 10);
+Ultrasonic u1(11, 12);
+
+void setup()
+{
+	Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(8, OUTPUT);
+  digitalWrite(8, LOW);
+}
+
+void loop()
+{
+  int dat1 = u1.distanceRead();
+  //int dat2 = u2.distanceRead();
+	if (Serial.available() > 0) {
+    //Do nothing  
+	}
+  Serial.println(dat1);
+  //Serial.print(" ");
+  //Serial.println(dat2);
+  
+}
